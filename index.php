64414c2a2cd3737b33ce6257fbb8d469de032a61
CristhianV15 <cmvc99@gmail.com>
2021-09-29 21:47:16 -0500

    Index mejorado

--- index.php
+++ index.php
@@ -20,22 +20,26 @@
 
 		<div class="nosotros-contenido">
 			<div class="bloque">
-				<img src="imagenes/Precio.png" width="20%">
+				<img src="imagenes/FotosInicio/todosDispositivos.png" width="75%">
 				<h3>No pierdas tus notas</h3>
-				<p>Lorem ipsum dolor sit amet, aliqua. Excepteur sint occaecat tu tu tu
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Al crear tu cuenta, podrás ver o actualizar tus notas desde tu computadora y continuarlos desde tu navegador preferido
+				   en tu celular. Además podras desactivar tu cuenta sin perder todas tus notas creadas. 										
+				</p>
 			</div>
 			<div class="bloque">
 				<img src="imagenes/Calidad.png" width="20%">
 				<h3>Fácil de usar</h3>
-				<p>Lorem ipsum dolor sit amet, aliqua. Excepteur sint occaecat tu tu tu
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Gracias a su interfaz simple podras crear, modificar, actualizar o eliminar tus notas al alcance de un click, también tendrás
+					tus ultimas notas fijadas en la parte superior para que no te olvides de nada. 
+
+				</p>
 			</div>
 			<div class="bloque">
 				<img src="imagenes/Garantia.png" width="20%">
-				<h3>Usalo desde tu móvil</h3>
-				<p>Lorem ipsum dolor sit amet, aliqua. Excepteur sint occaecat tu tu tu
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h3>Te damos algunas ideas de uso</h3>
+				<p>Puedes visitar nuestros ejemplos de texto en la parte superior, recuerda que UTP Notas
+					además de guardar tus apuntes de la universidad, también puede crear listas de tareas, de compras, agendar reuniones o ideas para tu nuevo proyecto. Que esperas para crear tu cuenta ahora.
+				</p>
 			</div>
 		</div>	
 	</section>
